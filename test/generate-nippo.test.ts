eb4ca3dc7bf5d0576a420611cff838cdaf17b109
Dz0526 <this@dz99.me>
2023-04-14 18:31:05 +0900

    Initial commit

---
+++ test/generate-nippo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GenerateNippo from '../lib/generate-nippo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/generate-nippo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GenerateNippo.GenerateNippoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
